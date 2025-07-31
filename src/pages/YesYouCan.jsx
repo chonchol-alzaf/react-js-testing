ff58ae405672eed65fa29431e1416ce58106b4a7
Chonchol <chonchol@alzaf.com>
2025-07-31 10:50:28 +0600

    changes

---
+++ src/pages/YesYouCan.jsx
@@ -0,0 +1,12 @@
+// src/pages/YesYouCan.jsx
+import React from 'react';
+
+const YesYouCan = () => {
+  return (
+    <div style={{ fontSize: '2rem', textAlign: 'center', marginTop: '20vh' }}>
+      <strong>Yea, you can.</strong>
+    </div>
+  );
+};
+
+export default YesYouCan;
